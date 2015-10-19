b497cdf4ce599bd17cb6cc56cf5709fbeb0ca485
octavianrotari <octavianrotari@gmail.com>
2015-10-19 16:10:59 +0100

    default balance 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,8 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  it {is_expected.to respond_to(:balance)}
+  it 'has a default value of 0' do
+    expect(subject.balance).to eq(Oystercard::DEFAULT_BALANCE)
+  end
 end
