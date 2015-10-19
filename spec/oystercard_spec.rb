2c05637acea3676c29893864d7c274a63dd37313
hasulica <raduasandei@gmail.com>
2015-10-19 16:16:03 +0100

    added topping up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,17 @@
 require 'oystercard'
 
 describe Oystercard do
+
   it {is_expected.to respond_to(:balance)}
+
   it 'has a default value of 0' do
     expect(subject.balance).to eq(Oystercard::DEFAULT_BALANCE)
   end
+
+  it 'adds money to balance' do
+    initial_balance = subject.balance
+    subject.top_up(20)
+    expect(subject.balance).to eq initial_balance + 20
+  end
+
 end
